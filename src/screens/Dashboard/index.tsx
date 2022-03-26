1e42541cf8e4422802d941e783f995ad96975836
henriquechagasg <henriquechagasg@gmail.com>
2022-03-26 16:43:26 -0300

    Styled Components: utilizando ícones

--- src/screens/Dashboard/index.tsx
+++ src/screens/Dashboard/index.tsx
@@ -1,10 +1,35 @@
 import React from 'react';
-import { Container, Title } from './styles';
+import {
+  Container,
+  Header,
+  UserInfoWrapper,
+  UserInfo,
+  Photo,
+  User,
+  UserGreeting,
+  UserName,
+  Icon,
+} from './styles';
 
 export function Dashboard() {
   return (
     <Container>
-      <Title>Dashboard</Title>
+      <Header>
+        <UserInfoWrapper>
+          <UserInfo>
+            <Photo
+              source={{
+                uri: 'https://avatars.githubusercontent.com/u/69800139?v=4',
+              }}
+            />
+            <User>
+              <UserGreeting>Olá,</UserGreeting>
+              <UserName>Henrique</UserName>
+            </User>
+          </UserInfo>
+          <Icon name="power" />
+        </UserInfoWrapper>
+      </Header>
     </Container>
   );
 }
